3767ea3e764cbd058439fae6e3e538b66ad65d9f
aleksey <av.pavlow@gmail.com>
2020-07-18 18:20:59 +0300

    Подключены редуктор и роуты

--- resources/js/store/reducers.js
+++ resources/js/store/reducers.js
@@ -3,5 +3,6 @@ import { combineReducers } from 'redux'
 import auth from '../modules/auth/store/reduer'
 import user from '../modules/user/store/reducer'
 import articles from '../modules/article/store/reducer'
+import cryptocurrencies from '../modules/cryptocurrency/store/reducer'
 
-export default combineReducers({ auth, user, articles })
+export default combineReducers({ auth, user, articles, cryptocurrencies })
